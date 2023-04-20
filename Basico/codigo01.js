270863645681c7a27dc0fa510933b76ef6bcb2e7
jfelipe <jfmoncada@udistrital.edu.co>
2023-04-20 15:53:21 -0500

    clase 01 JavaScript

---
+++ Basico/codigo01.js
@@ -0,0 +1,14 @@
+console.log('Hola Mundo!')
+var varhola = 'texto1'
+let var01 = 'texto2'
+var varhola = 'texto3'
+
+//mutabilidad
+varhola = 'Hola Mundo '
+var01 = 'JavaScript'
+console.log(varhola + var01)
+
+//Declaración de constantes
+const PI = 3.1416
+console.log(PI)
+
