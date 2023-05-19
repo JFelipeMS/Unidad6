0af4c7adbd1a2a7baefdc7c65e2f13055f3e5972
jfelipe <jfmoncada@udistrital.edu.co>
2023-05-18 20:40:59 -0500

    clase AJAX 02 JavaScript

---
+++ Ejercicios/Ajaxeje01.js
@@ -0,0 +1,30 @@
+var boton = document.getElementById("miboton")
+boton.addEventListener('click',ejecutarAJAX)
+var salida = document.getElementById("infoajax")
+
+function ejecutarAJAX(){
+    var peticionHTTP
+    if (window.XMLHttpRequest){
+        peticionHTTP = new XMLHttpRequest()
+    } else {
+        peticionHTTP = new ActiveXObject("Microsoft.XMLHTTP")
+    }
+
+    peticionHTTP.onreadystatechange = function() {
+    // 1 -> peticion se ha establecido
+    // 2 -> peticion se ha enviado
+    // 3 -> peticion esta siendo procesada
+    // 4 -> peticion ha sido finalizada
+        console.log(peticionHTTP.readyState)
+        if (peticionHTTP.readyState == 4)
+            if(peticionHTTP.status == 200){
+                var datos = JSON.parse(peticionHTTP.responseText)
+                console.log(datos)
+                //salida.innerHTML = ""
+            }
+    }
+    
+    peticionHTTP.open("GET","https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0",true)
+    peticionHTTP.send()
+    
+}
